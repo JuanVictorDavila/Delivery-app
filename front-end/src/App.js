0a0a2f4c81c2f43a577743fd83d1543c6910b6c4
juan victor <juanvictor70@gmail.com>
2022-07-12 16:32:08 -0400

    update: baixando atualizações da main

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+// import rockGlass from './images/rockGlass.svg';
+import Routes from './routes';
 
 function App() {
   return (
     <div className="App">
-      <span className="logo">TRYBE</span>
+      <h2>Welcome to the homepage!</h2>
+      <p>You can do this, I believe in you.</p>
+      {/* <span className="logo">TRYBE</span>
       <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
         Glass
-      </object>
+      </object> */}
+      <Routes />
     </div>
   );
 }
